7b501f6c0ff8256d162ebc3fef2c5fc3a0d37a7a
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-11-17 12:01:46 +0100

    Using a module to provide the functionality

---
+++ convert.pm
@@ -0,0 +1,30 @@
+package convert;
+
+sub convert
+{
+
+        my ($filename, $from, $to) = @_;
+
+unless( defined $filename && -e $filename)
+{
+        print "You need to specify one input filename as single parameter\n";
+        exit 1;
+}
+
+open(FH, "<", $filename) || die "Unable to open the file '$filename': $!";
+while(!eof(FH))
+{
+        my ($header, $seq, $header2, $qual) = (scalar <FH>, scalar <FH>, scalar <FH>, scalar <FH>);
+
+        # delete trailing newline from $quality line
+        chomp($qual);
+
+        # create new quality line
+        $qual = join("", map {chr(ord($_)-$from+$to)} (split(//, $qual)));
+
+        print $header, $seq, $header2, $qual, "\n";
+}
+close(FH) || die "Unable to close file '$filename': $!";
+}
+
+1;
--- fastq_33_to_64.pl
+++ fastq_33_to_64.pl
@@ -3,27 +3,8 @@
 use strict;
 use warnings;
 
+use convert;
+
 my $filename = shift @ARGV;
 
-if (! defined $filename || -e $filename)
-{
-	print "You need to specify one input filename as single parameter\n";
-	exit 1;
-}
-
-open(FH, "<", $filename) || die "Unable to open the file '$filename': $!";
-while(!eof(FH))
-{
-	my ($header, $seq, $header2, $qual) = (scalar <FH>, scalar <FH>, scalar <FH>, scalar <FH>);
-
-	# delete trailing newline from $quality line
-	chomp($qual);
-
-	# create new quality line
-	$qual = join("", map {chr(ord($_)-33+64)} (split(//, $qual)));
-
-	print $header, $seq, $header2, $qual, "\n";
-}
-close(FH) || die "Unable to close file '$filename': $!";
-
-
+convert::convert($filename, 33, 64);
